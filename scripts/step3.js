c44f9d85afde33f77113852e23802fd88d837f0f
Ethan <github.xdgcd@passmail.net>
2024-02-02 11:01:32 +0100

    :bento::sparkles: Step 3

---
+++ scripts/step3.js
@@ -0,0 +1,14 @@
+const loadingImage = document.querySelector(".loading");
+const ia = document.querySelector(".ia");
+const iaNeutral = document.querySelector(".ia-neutral");
+const iaTalking = document.querySelector(".ia-talking");
+
+setTimeout(() => {
+  loadingImage.style.display = "none";
+  ia.style.display = "flex";
+  iaNeutral.style.display = "flex";
+
+  setTimeout(() => {
+    window.location.href = "step4.html";
+  }, 1000);
+}, 3000);

167a7eb260fed1ac242810258e7ab4ecc8b6ec5a
mdorizon <mdorizon17@gmail.com>
2024-02-05 14:39:07 +0100

    :bug: speechsynthesis abandon fixed

--- scripts/step2.js
+++ scripts/step2.js
@@ -15,16 +15,24 @@ function vocalQuestionAssistant(file) {
 }
 
 function vocalResponseRecordUser() {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     var recognition = new webkitSpeechRecognition();
     recognition.lang = "fr-FR";
+    let response = "";
 
     recognition.onresult = function (event) {
-      var response = event.results[0][0].transcript;
-      response.toLowerCase();
+      let responseEvent = event.results[0][0].transcript;
+      responseEvent.toLowerCase();
+      response = responseEvent
       resolve(response);
     };
-
+    setTimeout(() => {
+      recognition.onspeechend = function (event) {
+        if(response === ""){
+          reject()
+        }
+      };
+    }, 1000);
     recognition.start();
   });
 }
@@ -34,22 +42,23 @@ async function initGame() {
   // demander pseudo
   await vocalQuestionAssistant("step2_identity.mp3");
   // écouter pseudo user
-  const pseudoAsk = await vocalResponseRecordUser();
-  pseudoField.textContent = pseudoAsk;
-  // confirmation pseudo
-  await vocalQuestionAssistant("step2_confirmation.mp3");
-  // écouter confirmation
-  const confirmAsk = await vocalResponseRecordUser();
-  if (
-    confirmAsk.includes("oui") ||
-    confirmAsk.includes("ouais") ||
-    confirmAsk.includes("ouaip")
-  ) {
-    const pseudo = pseudoAsk;
-    setTimeout(function () {
-      redirectToNextPage();
-    }, 15000);
-  } else {
+  try {
+    const pseudoAsk = await vocalResponseRecordUser();
+    pseudoField.textContent = pseudoAsk;
+    // confirmation pseudo
+    await vocalQuestionAssistant("step2_confirmation.mp3");
+    // écouter confirmation
+    const confirmAsk = await vocalResponseRecordUser();
+    if (confirmAsk.includes("oui") || confirmAsk.includes("ouais") || confirmAsk.includes("ouaip")) {
+      const pseudo = pseudoAsk;
+      setTimeout(function () {
+        redirectToNextPage();
+      }, 1500);
+    } else {
+      initGame();
+    }
+  } catch (error) {
+    console.log("veuillez répondre !")
     initGame();
   }
 }
